703c562eb61a9873494c1040336ffce0b974c524
xiaoyanjingjie <18641241027@163.com>
2020-07-07 12:38:20 +0800

    添加dubbo消费者功能 添加business 扫描包

---
+++ practice-business/src/main/java/jie/dian/wan/business/PracticeBusinessConfiguration.java
@@ -0,0 +1,18 @@
+package jie.dian.wan.business;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 启动类
+ *
+ * @author wan dianjie
+ * @date 2019-03-07 18:59
+ */
+@Configuration
+@ComponentScan("jie.dian.wan.business")
+@AllArgsConstructor
+public class PracticeBusinessConfiguration {
+
+}
